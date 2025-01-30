8056649f1044bedda3f80858cf902e0a808f0353
Jakub Kowal <ikubutek@gmail.com>
2025-01-30 01:35:11 +0100

    Chyba done Z3 juz nie wiem jak sie nazywam

--- MPIS/HW4Z3/src/main/java/lib/test/Main.java
+++ MPIS/HW4Z3/src/main/java/lib/test/Main.java
@@ -66,10 +66,10 @@ public class Main extends ApplicationFrame {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
 
         double p = 0;
-        int[] tab = new int[20];
-        for(int k = 0; k<=5000; k++) {
+        double[] tab = new double[20];
+        for(int k = 0; k<5000; k++) {
+            //tab[k] = function(n);
             double result = function(n);
-            //System.out.println(result);
             if (result >= 0 && result < 0.05) {
                 tab[0]++;
             } else if (result >= 0.05 && result < 0.10) {
@@ -112,33 +112,39 @@ public class Main extends ApplicationFrame {
                 tab[19]++;
             }
         }
-        int counter = 0;
-        for(int i = 0; i<20; i++){
-            counter+=tab[i];
+
+        // Normalizacja - obliczamy PDF
+        double[] pdf = new double[20];
+        for (int i = 0; i < 20; i++) {
+            pdf[i] = (double) tab[i] / (5000.0); // Normalizacja do PDF
+        }
+
+        dataset.addValue(pdf[0], "Pn", "0.05>Pn>=0");
+        dataset.addValue(pdf[1], "Pn", "0.1>Pn>=0.05");
+        dataset.addValue(pdf[2], "Pn", "0.15>Pn>=0.1");
+        dataset.addValue(pdf[3], "Pn", "0.2>Pn>=0.15");
+        dataset.addValue(pdf[4], "Pn", "0.25>Pn>=0.2");
+        dataset.addValue(pdf[5], "Pn", "0.3>Pn>=0.25");
+        dataset.addValue(pdf[6], "Pn", "0.35>Pn>=0.3");
+        dataset.addValue(pdf[7], "Pn", "0.4>Pn>=0.35");
+        dataset.addValue(pdf[8], "Pn", "0.45>Pn>=0.4");
+        dataset.addValue(pdf[9], "Pn", "0.5>Pn>=0.45");
+        dataset.addValue(pdf[10], "Pn", "0.55>Pn>=0.5");
+        dataset.addValue(pdf[11], "Pn", "0.6>Pn>=0.55");
+        dataset.addValue(pdf[12], "Pn", "0.65>Pn>=0.6");
+        dataset.addValue(pdf[13], "Pn", "0.7>Pn>=0.65");
+        dataset.addValue(pdf[14], "Pn", "0.75>Pn>=0.7");
+        dataset.addValue(pdf[15], "Pn", "0.8>Pn>=0.75");
+        dataset.addValue(pdf[16], "Pn", "0.85>Pn>=0.8");
+        dataset.addValue(pdf[17], "Pn", "0.9>Pn>=0.85");
+        dataset.addValue(pdf[18], "Pn", "0.95>Pn>=0.9");
+        dataset.addValue(pdf[19], "Pn", "1.0>Pn>=0.95");
+
+        double counter = 0;
+        for(double i: pdf){
+            counter+=i;
         }
         System.out.println(counter);
-
-        dataset.addValue(tab[0], "Pn", "0.05>Pn>=0");
-        dataset.addValue(tab[1], "Pn", "0.1>Pn>=0.05");
-        dataset.addValue(tab[2], "Pn", "0.15>Pn>=0.1");
-        dataset.addValue(tab[3], "Pn", "0.2>Pn>=0.15");
-        dataset.addValue(tab[4], "Pn", "0.25>Pn>=0.2");
-        dataset.addValue(tab[5], "Pn", "0.3>Pn>=0.25");
-        dataset.addValue(tab[6], "Pn", "0.35>Pn>=0.3");
-        dataset.addValue(tab[7], "Pn", "0.4>Pn>=0.35");
-        dataset.addValue(tab[8], "Pn", "0.45>Pn>=0.4");
-        dataset.addValue(tab[9], "Pn", "0.5>Pn>=0.45");
-        dataset.addValue(tab[10], "Pn", "0.55>Pn>=0.5");
-        dataset.addValue(tab[11], "Pn", "0.6>Pn>=0.55");
-        dataset.addValue(tab[12], "Pn", "0.65>Pn>=0.6");
-        dataset.addValue(tab[13], "Pn", "0.7>Pn>=0.65");
-        dataset.addValue(tab[14], "Pn", "0.75>Pn>=0.7");
-        dataset.addValue(tab[15], "Pn", "0.8>Pn>=0.75");
-        dataset.addValue(tab[16], "Pn", "0.85>Pn>=0.8");
-        dataset.addValue(tab[17], "Pn", "0.9>Pn>=0.85");
-        dataset.addValue(tab[18], "Pn", "0.95>Pn>=0.9");
-        dataset.addValue(tab[19], "Pn", "1.0>Pn>=0.95");
-
 
         Main app = new Main("HW4_3 Pn dla n = "+n);
         JFreeChart barChart = ChartFactory.createBarChart(
